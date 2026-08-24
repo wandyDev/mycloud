07a869e60503f8e5a3261595ce728e8f318d626e
wandyDev <wandydev@users.noreply.github.com>
2026-08-24 15:32:08 -0400

    regresando cors a la normalidad

--- apps/frontend/src/api/api.ts
+++ apps/frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api/v1",
+  baseURL: "http://api.wandycruz.me/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
