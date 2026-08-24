41349d5559e29464a46450b35ceb65cb5b8526f2
wandyDev <wandydev@users.noreply.github.com>
2026-08-24 15:00:51 -0400

    cambio temporal en los cors para probar vps

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: '*',
     credentials: true,
   });
 
