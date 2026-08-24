1c736ed3cc728d22ed0834133cc0117f05c6647b
wandyDev <wandydev@users.noreply.github.com>
2026-08-24 15:53:55 -0400

    desplegando frontend

--- apps/frontend/src/api/api.ts
+++ apps/frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://api.wandycruz.me/api/v1",
+  baseURL: "https://api.wandycruz.me/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
